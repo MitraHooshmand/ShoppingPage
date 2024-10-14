ba59cbef52e5ce339642e7a1c5bfb68c46f6a9d7
Mitra HSH <mitrahooshmand4824@gmail.com>
2024-10-14 13:40:27 +0330

    search by category, using event elegation

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+const allNodes = document.querySelectorAll(".product-item");
+
+////////////////////////////////////////// search by input
 document.getElementById("search-input").addEventListener("keyup", function (e) {
   const inputParam = e.target.value.toLowerCase().trim();
-  const allNodes = document.querySelectorAll(".product-item");
   allNodes.forEach((product) => {
     const productName = product.children[1].innerText.toLowerCase();
     if (productName.includes(inputParam)) {
@@ -8,5 +10,19 @@ document.getElementById("search-input").addEventListener("keyup", function (e) {
     } else {
       product.style.display = "none";
     }
+  });
+});
+////////////////////////////////////////// search by category using Event elegation
+
+const buttonsParents = document.querySelector(".buttonsParents");
+buttonsParents.addEventListener("click", (event) => {
+  const categoryKeyword = event.target.childNodes[0].textContent.toLowerCase();
+  allNodes.forEach((item) => {
+    if (categoryKeyword === "all") {
+      item.style.display = "block";
+    } else
+      categoryKeyword === item.dataset.category
+        ? (item.style.display = "block")
+        : (item.style.display = "none");
   });
 });
