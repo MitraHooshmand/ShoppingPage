e87ce9e67a933a5eeead5b09e20f0f072c89cbb9
Mitra HSH <mitrahooshmand4824@gmail.com>
2024-10-14 10:13:47 +0330

    search based on the input value is implemented

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-
+document.getElementById("search-input").addEventListener("keyup", function (e) {
+  const inputParam = e.target.value.toLowerCase().trim();
+  const allNodes = document.querySelectorAll(".product-item");
+  allNodes.forEach((product) => {
+    const productName = product.children[1].innerText.toLowerCase();
+    if (productName.includes(inputParam)) {
+      product.style.display = "block";
+    } else {
+      product.style.display = "none";
+    }
+  });
+});
